c94c102d697c4d80e0ff10e430b4a8dbe7c33488
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-11-17 00:00:34 -0500

    Fix schema invoices payment_terms type to int, invoice_items seeds price changed to cents

--- backend/db/seeds/03_invoice_items.sql
+++ backend/db/seeds/03_invoice_items.sql
@@ -1,11 +1,11 @@
 INSERT INTO invoice_items (name, quantity, price, total, invoice_id)
 VALUES
-  ('Brand Guidelines', 1, 1800.90, 1800.90, 1),
-  ('Banner Design', 1, 156.00, 156.00, 2),
-  ('Email Design', 2, 200.00, 400.00, 2),
-  ('Website Redesign', 1, 14002.33, 14002.33, 3),
-  ('Logo Sketches', 1, 102.04, 102.04, 4),
-  ('New Logo', 1, 1532.33, 1532.33, 5),
-  ('Brand Guidelines', 1, 2500.00, 2500.00, 5),
-  ('Web Design', 1, 6155.91, 6155.91, 6),
-  ('Logo Re-design', 1, 3102.04, 3102.04, 7);
+  ('Brand Guidelines', 1, 180090, 180090, 1),
+  ('Banner Design', 1, 15600, 15600, 2),
+  ('Email Design', 2, 20000, 40000, 2),
+  ('Website Redesign', 1, 1400233, 1400233, 3),
+  ('Logo Sketches', 1, 10204, 10204, 4),
+  ('New Logo', 1, 153233, 153233, 5),
+  ('Brand Guidelines', 1, 250000, 250000, 5),
+  ('Web Design', 1, 615591, 615591, 6),
+  ('Logo Re-design', 1, 310204, 310204, 7);
