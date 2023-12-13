75c411dfc18f91a7b1fd38b35a551bf878280f88
ascotlan <antonio.k.scotland@gmail.com>
2023-12-12 22:33:50 -0500

    update database reset script

--- backend/db/seeds/00_development.sql
+++ backend/db/seeds/00_development.sql
@@ -1,16 +1,17 @@
 INSERT INTO users (name, password, email, user_type, picture_url, street, city, postal_code, country, phone_number)
 VALUES
-  ('Business User', 'hashed_password_1', 'business@mail.com', 'business', 'https://i.pravatar.cc/60', '19 Union Terrace', 'London', 'E1 3EZ', 'United Kingdom', ''),
-  ('Jensen Huang', 'hashed_password_2', 'jensenh@mail.com', 'customer', 'https://i.pravatar.cc/60', '106 Kendell Street', 'Sharrington', 'NR24 5WQ', 'United Kingdom', '+3065021204'),
-  ('Alex Grim', 'hashed_password_3', 'alexgrim@mail.com', 'customer', 'https://i.pravatar.cc/60', '84 Church Way', 'Bradford', 'BD1 9PB', 'United Kingdom', '+5197224943'),
-  ('John Morrison', 'hashed_password_4', 'jm@myco.com', 'customer', 'https://i.pravatar.cc/60', '79 Dover Road', 'Westhall', 'IP19 3PF', 'United Kingdom', '+4170000000'),
-  ('Alysa Werner', 'hashed_password_5', 'alysa@email.co.uk', 'customer', 'https://i.pravatar.cc/60', '63 Warwick Road', 'Carlisle', 'CA20 2TG', 'United Kingdom', '+5194111111'),
-  ('Mellisa Clarke', 'hashed_password_6', 'mellisa.clarke@example.com', 'customer', 'https://i.pravatar.cc/60', '46 Abbey Row', 'Cambridge', 'CB5 6EG', 'United Kingdom', '+3065021111'),
-  ('Thomas Wayne', 'hashed_password_7', 'thomas@dc.com', 'customer', 'https://i.pravatar.cc/60', '3964 Queens Lane', 'Gotham', '60457', 'United States of America', '5191231234'),
-  ('Anita Wainwright', 'hashed_password_8', '', 'customer', 'https://i.pravatar.cc/60', '', '', '', '', '+4372001234'),
-  ('TechCorp Solutions', 'hashed_password_9', 'techcorp@mail.com', 'business', 'https://i.pravatar.cc/60', '123 Tech Plaza', 'Tech City', 'TECH123', 'Techland', '+5021231241'),
-  ('John Techman', 'hashed_password_10', 'john.techman@mail.com', 'customer', 'https://i.pravatar.cc/60', '456 Circuit Lane', 'Digitalburg', 'DIGI456', 'Techland', '+4371111112'),
-  ('Linda Coderella', 'hashed_password_11', 'linda.coderella@mail.com', 'customer', 'https://i.pravatar.cc/60', '789 Code Street', 'Codeville', 'CODE789', 'Techland', '+3061111115');
+  ('Business User', 'hashed_password_1', 'business@mail.com', 'business', 'https://i.pravatar.cc/60', '19 Union Terrace', 'London', 'E1 3EZ', 'United Kingdom', '+15555555555'),
+  ('Jensen Huang', 'hashed_password_2', 'jensenh@mail.com', 'customer', 'https://i.pravatar.cc/60', '106 Kendell Street', 'Sharrington', 'NR24 5WQ', 'United Kingdom', '+13065021204'),
+  ('Alex Grim', 'hashed_password_3', 'alexgrim@mail.com', 'customer', 'https://i.pravatar.cc/60', '84 Church Way', 'Bradford', 'BD1 9PB', 'United Kingdom', '+15197224943'),
+  ('John Morrison', 'hashed_password_4', 'jm@myco.com', 'customer', 'https://i.pravatar.cc/60', '79 Dover Road', 'Westhall', 'IP19 3PF', 'United Kingdom', '+14170000000'),
+  ('Alysa Werner', 'hashed_password_5', 'alysa@email.co.uk', 'customer', 'https://i.pravatar.cc/60', '63 Warwick Road', 'Carlisle', 'CA20 2TG', 'United Kingdom', '+15194111111'),
+  ('Mellisa Clarke', 'hashed_password_6', 'mellisa.clarke@example.com', 'customer', 'https://i.pravatar.cc/60', '46 Abbey Row', 'Cambridge', 'CB5 6EG', 'United Kingdom', '+13065021111'),
+  ('Thomas Wayne', 'hashed_password_7', 'thomas@dc.com', 'customer', 'https://i.pravatar.cc/60', '3964 Queens Lane', 'Gotham', '60457', 'United States of America', '+15191231234'),
+  ('Anita Wainwright', 'hashed_password_8', '', 'customer', 'https://i.pravatar.cc/60', '', '', '', '', '+14372001234'),
+  ('TechCorp Solutions', 'hashed_password_9', 'techcorp@mail.com', 'business', 'https://i.pravatar.cc/60', '123 Tech Plaza', 'Tech City', 'TECH123', 'Techland', '+15021231241'),
+  ('John Techman', 'hashed_password_10', 'john.techman@mail.com', 'customer', 'https://i.pravatar.cc/60', '456 Circuit Lane', 'Digitalburg', 'DIGI456', 'Techland', '+14371111112'),
+  ('Linda Coderella', 'hashed_password_11', 'linda.coderella@mail.com', 'customer', 'https://i.pravatar.cc/60', '789 Code Street', 'Codeville', 'CODE789', 'Techland', '+13061111115'),
+  ('Antonio Scotland', 'hashed_password_11', 'antonio.k.scotland@gmail.com', 'customer', 'https://i.pravatar.cc/60', '444 Connaught Street', 'Kitchener', 'N2C 1C3', 'Canada', '+15197224943');
 
 INSERT INTO invoices (invoice_number, created_at, description, status, payment_terms, due_date, business_user_id, customer_user_id)
 VALUES
