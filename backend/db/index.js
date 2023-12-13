4435635d7aaee43e7697d5695a023fe1e6b7e6ca
ascotlan <antonio.k.scotland@gmail.com>
2023-12-13 00:00:22 -0500

    Implement the connection URL approach for creating a connection pool to the postgreSQL db

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,14 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+
+// Construct connection string using environment variables
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL || connectionString,
 });
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
   // Attempt to safely release the client back to the pool
   if (client) {
     client.release();
@@ -17,11 +16,14 @@ pool.on('error', (err, client) => {
 });
 
 // Test the connection
-pool.query('SELECT NOW()', (err, res) => {
+pool.query("SELECT NOW()", (err, res) => {
   if (err) {
-    console.error('Error executing query', err.stack);
+    console.error("Error executing query", err.stack);
   } else {
-    console.log('Successful connection to the database established at:', res.rows[0].now);
+    console.log(
+      "Successful connection to the database established at:",
+      res.rows[0].now
+    );
   }
 });
 
