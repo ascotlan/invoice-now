a7428240babfba03d2aa0f388b079ec747554f02
ascotlan <antonio.k.scotland@gmail.com>
2023-12-14 01:12:36 -0500

    Add console.log for testing

--- backend/util/userSessionHelper.js
+++ backend/util/userSessionHelper.js
@@ -1,6 +1,7 @@
 const { UserNotAuthorizedError } = require("./errorHelper");
 
 const validateUserSession = (req, res, next) => {
+  console.log(req.session.user_id);
   const userId = req.session.user_id;
   if (!userId) {
     throw new UserNotAuthorizedError('User ID not found in session.', 401);
